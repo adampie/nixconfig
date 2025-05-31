9840f96e9c932af5b68cf7dadb6e2eb67d1a3cc9
Adam Pietrzycki <adam@pietrzycki.com>
2025-06-01 00:36:31 +0100

    test

--- flake.nix
+++ flake.nix
@@ -3,35 +3,36 @@
 
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
-
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
     nixpkgs-unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    # Add other inputs as needed
   };
 
-  outputs = {
-    self,
-    flake-schemas,
-    nixpkgs,
-    nixpkgs-unstable,
-  }: let
+  outputs = { self, flake-schemas, nixpkgs, nixpkgs-unstable, ... }@inputs: let
     supportedSystems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux"];
     forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
+      nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
   in {
     schemas = flake-schemas.schemas;
 
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          curl
-          git
-          jq
-          wget
-        ];
-      };
-    });
+    # --- System configurations (Darwin/macOS) ---
+    darwinConfigurations = {
+      "Adams-Macbook-Pro" = {};
+      "Adams-Work-Macbook-Pro" = {};
+    };
+
+    # --- Home Manager configurations ---
+    homeConfigurations = {
+      # Example stub:
+      # "adampie@Adams-Macbook-Pro" = {};
+    };
+
+    # --- Profiles (personal, work) ---
+    profiles = {
+      personal = {};
+      work = {};
+    };
   };
 }
