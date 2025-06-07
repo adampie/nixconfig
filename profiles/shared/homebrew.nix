30436d03a2ff76e7bd421f401afc09ceeb30403b
Adam Pietrzycki <adam@pietrzycki.com>
2025-06-07 22:20:04 +0100

    Add personal taps

--- profiles/shared/homebrew.nix
+++ profiles/shared/homebrew.nix
@@ -2,6 +2,19 @@
   homebrew = {
     enable = true;
 
+    taps = [
+      {
+        name = "adampie/homebrew-tap";
+        clone_target = "git@github.com:adampie/homebrew-tap.git";
+        force_auto_update = true;
+      }
+      {
+        name = "adampie/homebrew-tap-private";
+        clone_target = "git@github.com:adampie/homebrew-tap-private.git";
+        force_auto_update = true;
+      }
+    ];
+
     brews = [
       "mas"
     ];
