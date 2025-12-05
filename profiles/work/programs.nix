4b2e54241e3178a2ffc92deb14b1a5d4a408df50
Adam Pietrzycki <adam@pietrzycki.com>
2025-12-05 12:54:42 +0000

    Migrate to 25.11

--- profiles/work/programs.nix
+++ profiles/work/programs.nix
@@ -1,9 +1,10 @@
 _: {
   programs.git = {
-    extraConfig = {
-      commit = {gpgsign = true;};
-      gpg = {format = "ssh";};
-      "gpg.ssh" = {program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";};
+    settings = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      "gpg.ssh".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      user.email = "adam.pietrzycki@zapier.com";
     };
     includes = [
       {
@@ -16,7 +17,6 @@ _: {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEryumdeAtrZMZwW8DUPC6l4fhhS2B6ovLeX5QNynTTY";
       signByDefault = true;
     };
-    userEmail = "adam.pietrzycki@zapier.com";
   };
 
   programs.mise = {
