c6d79168fb6dced57c02ecddd81fa4de01b840cc
Adam Pietrzycki <adam@pietrzycki.com>
2026-03-26 23:40:07 +0000

    Add Zsh keybindings for word navigation and line editing

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -13,6 +13,14 @@
         };
         historySubstringSearch.enable = true;
         syntaxHighlighting.enable = true;
+        initContent = ''
+          bindkey '^[[1;5C' forward-word      # Ctrl+Right
+          bindkey '^[[1;5D' backward-word     # Ctrl+Left
+          bindkey '^[[H' beginning-of-line    # Home
+          bindkey '^[[F' end-of-line          # End
+          bindkey '^H' backward-kill-word     # Ctrl+Backspace
+          bindkey '^[[3;5~' kill-word         # Ctrl+Delete
+        '';
       };
     };
 }
