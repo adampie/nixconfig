8751e6266d46f397c66ee0deff04cee1aa53ad78
Adam Pietrzycki <adam@pietrzycki.com>
2026-04-06 21:17:50 +0100

    Update cache.adampie.dev public key

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -15,7 +15,7 @@
         "https://cache.nixos.org"
       ];
       nix.settings.trusted-public-keys = [
-        "cache.adampie.dev-1:njftfmru8p5NnYbVcfE22Tq0Ku+pEEplGs0nVqIGCUE="
+        "cache.adampie.dev-1:bYRS3Q1Jq7IaqK5wfN6Tw4Qeo51d1kV3YaPvpfEx+ek="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       nix.settings.auto-optimise-store = true;
