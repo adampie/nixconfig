45a34c0cc14aed09dad3402693aa79abc42bd779
Adam Pietrzycki <adam@pietrzycki.com>
2026-04-13 22:42:05 +0100

    Increase starship command_timeout to 1s to fix bun timeout warnings

--- modules/starship.nix
+++ modules/starship.nix
@@ -8,6 +8,7 @@
         enableZshIntegration = true;
         settings = {
           "$schema" = "https://starship.rs/config-schema.json";
+          command_timeout = 1000;
           add_newline = true;
           character.success_symbol = "[➜](bold green)";
           cmd_duration.format = "[ $duration]($style)";
