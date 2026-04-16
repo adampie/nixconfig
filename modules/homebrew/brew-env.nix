337a1f045a1c810d47730e8645b1a2ab93611c75
Adam Pietrzycki <adam@pietrzycki.com>
2026-04-17 00:34:07 +0100

    Disable Homebrew analytics
    Add HOMEBREW_NO_ANALYTICS to brew env config.

--- modules/homebrew/brew-env.nix
+++ modules/homebrew/brew-env.nix
@@ -4,6 +4,7 @@
     { ... }:
     {
       home.file.".homebrew/brew.env".text = ''
+        export HOMEBREW_NO_ANALYTICS=1
         export HOMEBREW_CASK_OPTS=--require-sha
         export HOMEBREW_NO_AUTO_UPDATE=1
         export HOMEBREW_NO_ENV_HINTS=1
