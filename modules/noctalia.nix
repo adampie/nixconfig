0a99a9953b03927346e82cb1fe535fd914865352
Adam Pietrzycki <adam@pietrzycki.com>
2026-04-27 21:26:58 +0100

    Add plugin support and privacy indicator settings to Noctalia configuration

--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -9,6 +9,22 @@
         enable = true;
         package = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
         settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
+        plugins = {
+          version = 2;
+          sources = [
+            {
+              enabled = true;
+              name = "Noctalia Plugins";
+              url = "https://github.com/noctalia-dev/noctalia-plugins";
+            }
+          ];
+          states = {
+            privacy-indicator = {
+              enabled = true;
+              sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+            };
+          };
+        };
       };
     };
 }
